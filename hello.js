3a9786b13ffa7784a091d466a7a9dbda53e33f21
Prabhat Singh <singhgprabhat1980@gmail.com>
2025-01-17 11:50:31 +0530

    hello created

---
+++ hello.js
@@ -0,0 +1,5 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var val1 = add(5, 6);
+console.log(val1);
---
+++ hello.ts
@@ -0,0 +1,6 @@
+function add(n1:number,n2:number){
+    return n1+n2;
+}
+
+const val1=add(5,6);
+console.log(val1)
